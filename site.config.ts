f32550c7bd523230fa5e0a4594b5f0b1d97ba1a5
shiyi9999 <133764421+shiyi9999@users.noreply.github.com>
2023-05-16 19:25:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'blog-0bd62998869446f3a129d4caad95199d',
+  rootNotionPageId: '0bd62998869446f3a129d4caad95199d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'blog',
-  domain: 'https://www.notion.so/shiyi1/blog-0bd62998869446f3a129d4caad95199d',
+  domain: 'https://www.notion.so/shiyi1/0bd62998869446f3a129d4caad95199d',
   author: 'shiyi',
 
   // open graph metadata (optional)
